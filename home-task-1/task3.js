5cfe245cefde08803324c74f2c824259beb98a6b
Yana Montashevych <49094064+yana-mon@users.noreply.github.com>
2021-08-25 09:13:42 +0300

    Update task3.js

--- home-task-1/task3.js
+++ home-task-1/task3.js
@@ -1,1 +1,5 @@
-
+function solveQuadraticEquation(a,b,c){
+    x1 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    x2 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    console.log(x1, x2);
+}
