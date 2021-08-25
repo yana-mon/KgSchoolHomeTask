d4aa09b12bd778129b40cd5d910eddfa202d756e
Yana Montashevych <49094064+yana-mon@users.noreply.github.com>
2021-08-25 09:12:54 +0300

    Update task1.js

--- home-task-1/task1.js
+++ home-task-1/task1.js
@@ -1,1 +1,12 @@
-
+function drawTriangle(height) {
+    let res = '';
+    for (let i = height; i > 0; i--) {
+        for (let j = height; j > 0; j--) {
+            if (i <= j){
+                res = res + '#';
+            }
+        }
+        console.log(`${res}\n`);
+        res = '';
+    }
+}
