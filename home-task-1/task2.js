860b16e65e4a6cd78013a6ab8b9f4b27968571ed
Yana Montashevych <49094064+yana-mon@users.noreply.github.com>
2021-08-25 09:13:21 +0300

    Update task2.js

--- home-task-1/task2.js
+++ home-task-1/task2.js
@@ -1,1 +1,14 @@
-
+function drawChessboard(){
+    let str = '';
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if ((!(i % 2) && (j % 2)) || ((i % 2) && !(j % 2))) {
+                str = str + 'b';
+            } else {
+                str = str + 'w';
+            }
+        }
+        console.log(`${str}\n`);
+        str = '';
+    }
+}
